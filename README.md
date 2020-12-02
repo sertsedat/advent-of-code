211a4b90e338b531c9e301f84e14b34f7d34d196
Sedat Sert <bmsedat@gmail.com>
2020-12-02 20:29:41 +0100

    advent of code day 1 with cargo-aoc

---
+++ README.md
@@ -0,0 +1,1 @@
+I'm learning Rust and I have decided to solve Advent of Code challenges in Rust.
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+extern crate aoc_runner;
+extern crate aoc_runner_derive;
+
+use aoc_runner_derive::aoc_main;
+
+aoc_main! { lib = advent_of_code_2020 }
