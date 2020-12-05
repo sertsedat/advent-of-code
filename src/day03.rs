cb5fe0e228f558571f377ddd7c05e8fe5b545e59
Sedat Sert <bmsedat@gmail.com>
2020-12-05 22:23:43 +0100

    add doc tests

--- src/day03.rs
+++ src/day03.rs
@@ -1,5 +1,3 @@
-// --- Day 3: Toboggan Trajectory ---
-
 #[aoc_generator(day3)]
 pub fn generate_input(input: &str) -> Vec<String> {
     input.lines().map(|line| line.to_string()).collect()
@@ -29,97 +27,23 @@ pub fn count_encountered_trees(input: &Vec<String>, col_step: &usize, row_step: 
     found_trees
 }
 
-/*
- * Part 1
- *
- * With the toboggan login problems resolved, you set off toward the airport.
- * While travel by toboggan might be easy, it's certainly not safe:
- * there's very minimal steering and the area is covered in trees.
- * You'll need to see which angles will take you near the fewest trees.
- *
- * Due to the local geology, trees in this area only grow on exact integer coordinates in a grid.
- * You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
- *
- * ..##.......
- * #...#...#..
- * .#....#..#.
- * ..#.#...#.#
- * .#...##..#.
- * ..#.##.....
- * .#.#.#....#
- * .#........#
- * #.##...#...
- * #...##....#
- * .#..#...#.#
- *
- * These aren't the only trees, though;
- * due to something you read about once involving arboreal genetics and biome stability,
- * the same pattern repeats to the right many times:
- *
- *
- * ..##.........##.........##.........##.........##.........##.......  --->
- * #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
- * .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
- * ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
- * .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
- * ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
- * .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
- * .#........#.#........#.#........#.#........#.#........#.#........#
- * #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
- * #...##....##...##....##...##....##...##....##...##....##...##....#
- * .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
- *
- * You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
- *
- * The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers);
- * start by counting all the trees you would encounter for the slope right 3, down 1:
- *
- * From your starting position at the top-left, check the position that is right 3 and down 1.
- * Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
- *
- * The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
- *
- * ..##.........##.........##.........##.........##.........##.......  --->
- * #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
- * .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
- * ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
- * .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
- * ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
- * .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
- * .#........#.#........X.#........#.#........#.#........#.#........#
- * #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
- * #...##....##...##....##...#X....##...##....##...##....##...##....#
- * .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
- *
- * In this example, traversing the map using this slope would cause you to encounter 7 trees.
- *
- * Starting at the top-left corner of your map and following a slope of right 3 and down 1,
- * how many trees would you encounter?
-*/
+/// ```
+/// use advent_of_code_2020::day03::*;
+/// use std::fs;
+/// let input = fs::read_to_string("input/2020/day3.txt").unwrap();
+/// assert_eq!(solve_part1(&generate_input(&input)), 286);
+/// ```
 #[aoc(day3, part1)]
 pub fn solve_part1(input: &Vec<String>) -> usize {
     count_encountered_trees(input, &3, &1)
 }
 
-/*
- * Part 2
- *
- * Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
- *
- * Determine the number of trees you would encounter if, for each of the following slopes,
- * you start at the top-left corner and traverse the map all the way to the bottom:
- *
- * Right 1, down 1.
- * Right 3, down 1. (This is the slope you already checked.)
- * Right 5, down 1.
- * Right 7, down 1.
- * Right 1, down 2.
- *
- * In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively;
- * multiplied together, these produce the answer 336.
- *
- * What do you get if you multiply together the number of trees encountered on each of the listed slopes?
- */
+/// ```
+/// use advent_of_code_2020::day03::*;
+/// use std::fs;
+/// let input = fs::read_to_string("input/2020/day3.txt").unwrap();
+/// assert_eq!(solve_part2(&generate_input(&input)), 3638606400);
+/// ```
 #[aoc(day3, part2)]
 pub fn solve_part2(input: &Vec<String>) -> usize {
     let slopes: Vec<(usize, usize)> = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
