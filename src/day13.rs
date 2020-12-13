e7fd07b5b5e7f22d1c7cbdb0596102244090179f
Sedat Sert <bmsedat@gmail.com>
2020-12-13 21:44:48 +0100

    separate aoc_generators for better performance

--- src/day13.rs
+++ src/day13.rs
@@ -1,31 +1,49 @@
-#[aoc_generator(day13)]
-pub fn generate_input(input: &str) -> (u64, Vec<String>) {
+#[aoc_generator(day13, part1)]
+pub fn generate_input_part1(input: &str) -> (u64, Vec<u64>) {
     let mut lines = input.lines();
     let timestamp: u64 = lines.next().unwrap().parse().unwrap();
+    let bus_lines: Vec<u64> = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .filter(|&l| l != "x")
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<u64>>();
+
+    (timestamp, bus_lines)
+}
+
+pub struct BusLine(u64, u64);
+
+#[aoc_generator(day13, part2)]
+pub fn generate_input_part2(input: &str) -> Vec<BusLine> {
+    let mut lines = input.lines();
+    lines.next();
     let bus_lines = lines
         .next()
         .unwrap()
-        .split(",")
-        .map(|x| x.to_string())
+        .split(',')
+        .enumerate()
+        .filter_map(|(i, line)| {
+            if line == "x" {
+                None
+            } else {
+                Some(BusLine(line.parse().unwrap(), i as u64))
+            }
+        })
         .collect();
-
-    (timestamp, bus_lines)
+    bus_lines
 }
 
 /// ```
 /// use advent_of_code_2020::day13::*;
 /// use std::fs;
 /// let input = fs::read_to_string("input/2020/day13.txt").unwrap();
-/// assert_eq!(solve_part1(&generate_input(&input)), 259);
+/// assert_eq!(solve_part1(&generate_input_part1(&input)), 259);
 /// ```
 #[aoc(day13, part1)]
-pub fn solve_part1((timestamp, bus_lines): &(u64, Vec<String>)) -> u64 {
-    let bus_lines: Vec<u64> = bus_lines
-        .iter()
-        .filter(|&l| l != "x")
-        .map(|l| l.parse().unwrap())
-        .collect();
-
+pub fn solve_part1((timestamp, bus_lines): &(u64, Vec<u64>)) -> u64 {
+    let bus_lines = bus_lines;
     let mut t = 0;
     loop {
         let earliest = bus_lines
@@ -40,28 +58,14 @@ pub fn solve_part1((timestamp, bus_lines): &(u64, Vec<String>)) -> u64 {
     }
 }
 
-struct BusLine(u64, u64);
-
 /// ```
 /// use advent_of_code_2020::day13::*;
 /// use std::fs;
 /// let input = fs::read_to_string("input/2020/day13.txt").unwrap();
-/// assert_eq!(solve_part2(&generate_input(&input)), 210612924879242);
+/// assert_eq!(solve_part2(&generate_input_part2(&input)), 210612924879242);
 /// ```
 #[aoc(day13, part2)]
-pub fn solve_part2((_, bus_lines): &(u64, Vec<String>)) -> u64 {
-    let bus_lines: Vec<BusLine> = bus_lines
-        .iter()
-        .enumerate()
-        .filter_map(|(i, line)| {
-            if line == "x" {
-                None
-            } else {
-                Some(BusLine(line.parse().unwrap(), i as u64))
-            }
-        })
-        .collect();
-
+pub fn solve_part2(bus_lines: &Vec<BusLine>) -> u64 {
     let mut t = 0;
     let mut denominator = 1;
 
@@ -87,7 +91,7 @@ mod tests {
         let text = "939
 7,13,x,x,59,x,31,19
 ";
-        let input = generate_input(text);
+        let input = generate_input_part1(text);
 
         let actual = solve_part1(&input);
         assert_eq!(actual, 295);
@@ -98,7 +102,7 @@ mod tests {
         let text = "939
 7,13,x,x,59,x,31,19
 ";
-        let input = generate_input(text);
+        let input = generate_input_part2(text);
 
         let actual = solve_part2(&input);
         assert_eq!(actual, 1068781);
@@ -109,7 +113,7 @@ mod tests {
         let text = "939
 17,x,13,19
 ";
-        let input = generate_input(text);
+        let input = generate_input_part2(text);
 
         let actual = solve_part2(&input);
         assert_eq!(actual, 3417);
@@ -120,7 +124,7 @@ mod tests {
         let text = "939
 67,7,59,61
 ";
-        let input = generate_input(text);
+        let input = generate_input_part2(text);
 
         let actual = solve_part2(&input);
         assert_eq!(actual, 754018);
